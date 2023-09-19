c332161f0f9f7bf01aab578b89b7aacb603ee80c
silviapetrino <petrino.silvia@gmail.com>
2023-09-19 12:08:02 +0200

    [init] commit

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # js-campominato-dom
+
+Creazione Griglia
+
+Griglia del Campo Minato. L’utente clicca su un bottone che genererà una griglia di gioco quadrata. Ogni cella ha un numero progressivo, da 1 a 100. Ci saranno quindi 10 caselle per ognuna delle 10 righe. Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+
+
+1. Imposto gli elementi statici in markup con html/css: un bottone, un select menu, un container per la griglia, i quadrati.
+
+2. in Javascript: 
+   - inserisco il container in una variabile, sarà l'ambiente in cui stampare i quadrati;
+  - prendo il bottone e creo una funzione per inizializzare il gioco;
+  - creo una funzione di reset per svuotare il container;
+  - Creo una funzione per generare un quadrato;
+  - creo un ciclo per stampare 100 volte il quadrato;
+  - inserisco il ciclo all'interno dell'evento click del bottone;
+
+ // bonus 
+
+1- creo una variabile per prendere l'id del select menu;
+
+2-  creo il markup degli altri quadrati (livello medio e livello difficile);
+
+3- creo altre due funzioni che generino quadrati diversi;
+
+4- creo altre due funzioni per inserirli dentro al container;
+
+5- tolgo il bottone da dentro la funzione creata per inizializzare il gioco semplice;
+
+6- creo delle condizioni: stabilisco che, cliccando il bottone, si inizializzi una funzione diversa seconda della scelta selezionata.
+
+___
+Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+In seguito l’utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle.
+La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba, fare visulaizzare tutte le bombe in gliglia e congelare la griglia.
+
+
+0. creo una classe Css bomba a cui dò un colore.
+1. creo una funzione per generare la bomba in un numero da 1 a 16 e inserisco un ciclo che gira finché non sono tutte uscite;
+
+2. lavoro sul click del quadrato
---
+++ js/script.js
@@ -0,0 +1,121 @@
+
+const button = document.getElementById("start-game");
+const container = document.querySelector(".container_sp");
+const gameChoise = document.getElementById("game-difficult");
+
+
+  // al click del bottone la scelta di gioco selezionata attiva la funzione corrispondente
+
+button.addEventListener("click", function () {
+  resetGame();
+  if (gameChoise.value === "easy") {
+    startGameEasy();
+  } else if (gameChoise.value === "medium") {
+    startGameMedium();
+  } else if (gameChoise.value === "hard") {
+    startGameHard();
+  }
+});
+
+
+// *********functions*************
+
+
+function resetGame() {
+  return container.innerHTML = " ";
+}
+
+
+function startGameEasy(){
+    resetGame()
+
+    for(i = 1; i < 101; i++) {
+      const square = createSquare(i);
+      container.append(square);
+      square.addEventListener("click", function(){
+      this.classList.toggle("active");
+      console.log(this.id)
+      })
+    }
+  }
+
+function startGameMedium(){
+
+    for(i = 1; i < 82; i++) {
+      const square = createSquareMedium(i);
+      container.append(square);
+      square.addEventListener("click", function(){
+      this.classList.toggle("active");
+      console.log(this.id);
+      })
+    } 
+  }
+
+  function startGameHard(){
+
+    for(i = 1; i < 50; i++) {
+      const square = createSquareHard(i);
+      container.append(square);
+      square.addEventListener("click", function(){
+      this.classList.toggle("active");
+      console.log(this.id);
+      })
+    } 
+  }
+
+
+
+  // i quadrati sono creati in base alla difficoltà del gioco scelta 
+
+function createSquare(index) {
+  const newSquare = document.createElement("div");
+  newSquare.classList.add("square-easy");
+  newSquare.innerHTML = "";
+  newSquare.id = index;
+  return newSquare;
+}
+
+
+function createSquareMedium(index) {
+  const newSquare = document.createElement("div");
+  newSquare.classList.add("square-medium");
+  newSquare.innerHTML = "";
+  newSquare.id = index;
+  return newSquare;
+}
+
+
+function createSquareHard(index) {
+  const newSquare = document.createElement("div");
+  newSquare.classList.add("square-hard");
+  newSquare.innerHTML = "";
+  newSquare.id = index;
+  return newSquare;
+}
+
+
+
+
+
+// 1. funzione per generare  un numero da 1 a 16: inserisco un ciclo che gira finché non sono usciti tutti i numeri, che aggiungo man mano alla lista;
+
+
+
+function randomBomb() {
+
+  const min = 0;
+  const max = 16;
+  const bombsList = [];
+
+  let numeroCasuale;
+
+  do {
+    numeroCasuale = Math.floor(Math.random() * (max - min + 1)) + min;
+  } while (bombsList.includes(numeroCasuale));
+
+  bombsList.push(numeroCasuale);
+
+  return bombsList; 
+ 
+}
+
